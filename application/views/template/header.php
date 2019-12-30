899c02efbd72da19561cf9ededa6f940977e88c3
yunanvupa <yunanvupa13@gmail.com>
2019-12-30 14:30:20 +0700

    menu program

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -254,7 +254,7 @@
 						<!-- text-->
 						<div class="dropdown-divider"></div>
 						<!-- text-->
-						<a href="login.html" class="dropdown-item"><i class="fa fa-power-off"></i> Logout</a>
+						<a href="<?php echo base_url('auth/logout');?>" class="dropdown-item"><i class="fa fa-power-off"></i> Logout</a>
 						<!-- text-->
 					</div>
 				</li>
